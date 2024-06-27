a680ebb150486a3d9d2654206487635702331b5e
eashw <eashw@eashwar>
2024-06-27 12:23:31 +0530

    updated to demo char

---
+++ javabasics1/src/day4/String1.java
@@ -0,0 +1,13 @@
+package day4;
+
+public class String1 {
+
+	public static void main(String[] args) {
+		System.out.println("wlecome to JFS");
+		System.out.println("mvit");
+		System.out.print("tnsif");
+		// TODO Auto-generated method stub
+
+	}
+
+}
